44ffc53a94da0f58097166b2b26c68d4dee335c3
Shibam <shibam.math@gmail.com>
2021-08-03 11:40:21 +0300

    Added smallKNOT

---
+++ smallKNOT/CP/permutation.py
@@ -0,0 +1,28 @@
+class Permutation:
+	def __init__(self, block_size):
+		self.__block_size = block_size
+		self.__row_size = block_size >> 2
+		
+		self.__rotation = self.set_rotation()
+		
+	def set_rotation(self):
+		if(self.__block_size == 256):
+			rotation = [0,1,8,25]
+		elif(self.__block_size == 384):
+			rotation = [0,1,8,55]
+		elif(self.__block_size == 512):
+			rotation = [0,1,16,25]
+		elif(self.__block_size == 32):
+			rotation = [0,1,5,7]
+		else:
+			print("Invalid_block_size")
+			exit();
+		return rotation
+	def gen_permutation(self):
+		states = [[x for x in range(self.__row_size)] for i in range(4)]
+		output_states = [[] for i in range(4)]
+		output_states[0] = states[0]
+		for row in range(1,4):
+			temp = states[row][self.__rotation[row]:] + states[row][:self.__rotation[row]]
+			output_states[row] = temp
+		return output_states
---
+++ smallKNOT/CP/test.py
@@ -0,0 +1,100 @@
+import pymzn
+from itertools import *
+import time 
+from smallKNOT import *
+import sys
+
+class autometic_search:
+	def __init__(self, cipher_name, sbox_list, block_size, number_of_sbox):
+		self.__model_file = cipher_name + '.mzn'
+		self.__model_data = cipher_name + '.dzn'
+		self.__result = cipher_name
+		File_model = open(self.__model_file, "w")
+		File_model.close()
+		self.__sbox_list = sbox_list
+		self.__block_size = block_size
+		self.__ns = number_of_sbox
+	
+	def generate_In(self, C):
+		d = 'round_0_X = array2d(0..3, 0..%d, [|'%(self.__ns - 1)
+		temp1 = [ ]
+		temp2 = [ ]
+		for row in range(0,4):
+			temp = [ ]
+			for col in range(0,self.__ns):
+					temp.append('true')
+			temp2.append(temp)
+		for c in C:
+			row = c[0]
+			col = c[1]
+			temp2[row][col] = 'false'
+		for row in range(0,4):
+			temp1.append(', '.join(temp2[row]))
+		d = d + '| '.join(temp1) + '|] );'				
+		return (d)			
+	def generate_Out(self, R, C):
+		d = 'round_%d_X = array2d(0..3, 0..%d, [|'%(R,self.__ns - 1)
+		temp1 = [ ]
+		for row in range(0,4):
+			temp = [ ]
+			for col in range(0,self.__ns):
+				if(row == C[0] and col == C[1]):
+					temp.append('true')
+				else:
+					temp.append('false')
+			temp1.append(', '.join(temp))
+		d = d + '| '.join(temp1) + '|] );'
+		return (d)
+	
+	def get_balanced_bits(self,Round,C):
+		KNOT= CIPHER_MODEL(self.__sbox_list, self.__block_size, Round, self.__ns)
+		model = '\n'.join(KNOT.get_full_constraints() )
+		temp_model = model + 'solve satisfy;' 
+		
+		
+		#print(temp_model)
+		File_model = open(self.__model_file, "w")
+		File_model.write(temp_model)
+		File_model.close()
+		
+		
+		time_start = time.time()
+		input_property = self.generate_In(C) 
+		result =[]
+		for out_row in range(0,4):
+			for j in range(0,self.__ns):	
+				B = []
+				B.append(out_row)
+				B.append(j)
+				#print(B)
+				input_data = input_property +'\n'+ self.generate_Out(Round, B)
+				R = pymzn.minizinc(temp_model, data = input_data, solver=pymzn.Chuffed(solver_id='chuffed'), parallel ='4')
+				if(str(R) == 'UNSATISFIABLE'):
+				    result.append(B)
+				    print ("Input=> ", C, " Output=> ",B, "***Balanced***")
+				else:
+					print ("Input=> ", C, " Output=> ",B, "***Unknown***")
+		time_end = time.time()
+		print("Time used = " + str(time_end - time_start))
+		return (result)
+
+if __name__ == '__main__':
+	sbox = [6, 0, 11, 5, 9, 4, 2, 7, 14, 8, 15, 1, 12, 13, 3, 10]
+	cipher_name = "smallKNOT"
+	
+	n = len(sys.argv)
+	block_size = int(sys.argv[1])
+	number_of_sbox = block_size >> 2
+	A = autometic_search(cipher_name, sbox, block_size, number_of_sbox)
+	number_of_rounds = int(sys.argv[2])
+	C = [ ]
+	for i in range(3,n):
+		st = sys.argv[i]
+		c_temp = [j for j in st.split(',')]
+		c = [int(j) for j in c_temp]
+		C.append(c)
+	# = [[3,0]] #sys.argv[3]
+	print (C)
+	res = A.get_balanced_bits(number_of_rounds,C)
+	print (res)
+	print ("Number of balanced bits with input ", C, " = ",len(res))
