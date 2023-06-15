9e469eea46b5485be68cac8112c06d7d8fa38387
Shibam <shibam.math@gmail.com>
2023-06-15 14:49:40 +0300

    ss

--- Ascon_MILP/ascon.cpp
+++ Ascon_MILP/ascon.cpp
@@ -101,9 +101,16 @@ void division_property_1(int rounds, vector<int> &active, vector<int> &not_balan
     env.set(GRB_IntParam_Threads, THREAD );
     
     GRBModel model = GRBModel(env);
-    //X0 - > SB -> Y0 -> L -> X1 -> SB -> Y1   
+    
+    //Z ->L-> X0
+    //X0 -> SB -> Y0 -> L -> X1 -> SB -> Y1 
+    vector<GRBVar> Z(STATE);
     vector<vector<GRBVar>> X(rounds, vector<GRBVar>(STATE));
     vector<vector<GRBVar>> Y(rounds, vector<GRBVar>(STATE));
+
+    for( int i = 0; i<STATE; i++){
+        Z[i] = model.addVar(0, 1, 0, GRB_BINARY);
+    }
     for( int r = 0 ; r<rounds; r++){
         for( int i = 0; i<STATE; i++){
             X[r][i] = model.addVar(0, 1, 0, GRB_BINARY);
@@ -111,6 +118,7 @@ void division_property_1(int rounds, vector<int> &active, vector<int> &not_balan
         }
     }
     
+    diffusion(model, Z, X[0]) ;
     for( int r = 0; r<rounds; r++){
         substitution(model, X[r], Y[r]) ;
         if(r != (rounds-1))
@@ -120,10 +128,10 @@ void division_property_1(int rounds, vector<int> &active, vector<int> &not_balan
     //input constraint
     for ( int i = 0; i < STATE; i++ ){
         if ( active[i] == 0 ){
-            model.addConstr( X[0][i] == 0 );
+            model.addConstr( Z[i] == 0 );
         }
         else{
-            model.addConstr( X[0][i] == 1 );
+            model.addConstr( Z[i] == 1 );
         }
     }
 
@@ -249,6 +257,66 @@ void division_property(int rounds, vector<int> &active, vector<int> &not_balance
     }
     cout<<endl;
 }
+int even_polynomial(int rounds, vector<int> &input, vector<int> &output){
+    GRBEnv env = GRBEnv();
+    env.set(GRB_IntParam_LogToConsole, 0);
+    env.set(GRB_IntParam_Threads, THREAD );
+    
+    GRBModel model = GRBModel(env);
+    //X0 - > SB -> Y0 -> L -> X1 -> SB -> Y1   
+    vector<vector<GRBVar>> X(rounds, vector<GRBVar>(STATE));
+    vector<vector<GRBVar>> Y(rounds, vector<GRBVar>(STATE));
+    for( int r = 0 ; r<rounds; r++){
+        for( int i = 0; i<STATE; i++){
+            X[r][i] = model.addVar(0, 1, 0, GRB_BINARY);
+            Y[r][i] = model.addVar(0, 1, 0, GRB_BINARY);
+        }
+    }
+    
+    for( int r = 0; r<rounds; r++){
+        substitution(model, X[r], Y[r]) ;
+        if(r != (rounds-1))
+            diffusion(model, Y[r], X[r+1]) ;
+    }
+	
+    //input constraint
+    for ( int i = 0; i < STATE; i++ ){
+        if ( input[i] == 0 ){
+            model.addConstr( X[0][i] == 0 );
+        }
+        else{
+            model.addConstr( X[0][i] == 1 );
+        }
+    }
+    //output constrait
+    for ( int i = 0; i < STATE; i++ ){
+        if ( output[i] == 1 ){
+            model.addConstr( Y[rounds-1][i] == 1 );
+        }
+        else{
+            model.addConstr( Y[rounds-1][i] == 0 );
+        }
+    }
+
+    model.write("./data/model.lp");
+    model.optimize();
+    int balanced = 0;
+    if( model.get( GRB_IntAttr_Status ) == GRB_OPTIMAL){
+        double time = model.get(GRB_DoubleAttr_Runtime );
+        cout << "Time Used: " << time << "sec" << endl;
+        /* print_trail(rounds, X, model); */
+    }
+    else if( model.get( GRB_IntAttr_Status ) == GRB_INFEASIBLE ){
+        cout << "Integral Distinguisher Found\n";
+        balanced = 1;
+    }
+    else{
+        cout << "Other status " << GRB_IntAttr_Status <<  endl;
+        exit(-1);
+    }
+    return balanced;
+}
+
 void set_input(vector<int> &active, int s, int e){
     for(int i=0; i<STATE; i++){
        active[i] = 1;
@@ -257,8 +325,41 @@ void set_input(vector<int> &active, int s, int e){
         active[constant_bits[i]] = 0;   
     }
 }
-void test(){
+void set_output(vector<int> &active, int *V, int sbox_no){
+    for(int i=0; i<STATE; i++){
+       active[i] = 0;
+    }
+    for(int i=0; i<2; i++){
+        active[(64*V[i]) + sbox_no] = 1;
+    }
+}
+void test_poly(){
     int rounds = 5;
+    vector<int> input(STATE);
+    set_input(input, 21, 320);
+
+    vector<int> output(STATE);
+
+    int V[10][2] = {{0,1},{0,2},{0,3},{0,4},{1,2},{1,3},{1,4},{2,3},{2,4},{3,4}};
+
+    for(int s=0; s<1; s++){
+        for(int i=0; i<10; i++){
+            set_output(output, V[i], s);
+            print_prop(input);
+            print_prop(output);
+            int balanced = even_polynomial(rounds, input, output);
+            cout << V[i][0] << " " << V[i][1] << ":";
+            if (balanced == 1){
+                cout << "balanced" << endl;
+            }
+            else{
+                cout << "unknown" << endl;
+            }
+        }
+    }
+}
+void test(){
+    int rounds = 6;
     vector<int > not_balanced;
     vector<int > active(STATE);
     set_input(active, 15, 320);
